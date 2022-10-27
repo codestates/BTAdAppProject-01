fc54e56f7a31e3a8000697c5415acd56de2c1c84
younghunban <younghunban@ui-MacBookPro.local>
2022-10-27 19:01:23 +0900

    init

---
+++ hype-dapp/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Navbar, Text, Avatar, Link } from "@nextui-org/react";
+import Explorer from "./pages/Explorer";
+import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
+import Minting from "./pages/Minting";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+function App() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="App">
+      <Navbar>
+        <Navbar.Brand>
+          <Text as={Link} href="/">
+            hypedApp
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content>
+          <Navbar.Link href="/#">Connect</Navbar.Link>
+          <Navbar.Link href="/minting">Create</Navbar.Link>
+          <Navbar.Item>
+            <Avatar as={Link} href="/my-page"></Avatar>
+          </Navbar.Item>
+        </Navbar.Content>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explorer" element={<Explorer />} />
+        <Route path="/minting" element={<Minting />} />
+        <Route path="/my-page" element={<MyPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ hype-dapp/src/pages/Minting/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Minting = () => {
+  return <div></div>;
+};
+
+export default Minting;
