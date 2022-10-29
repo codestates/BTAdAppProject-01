529ee60f40e4d3c7d4216635ff3a6260daccab7c
younghunban <younghunban@ui-MacBookPro.local>
2022-10-29 10:47:18 +0900

    feat: 지갑 연결 및 NFT 조회

---
+++ hype-dapp/src/constants/const.js
@@ -0,0 +1,2 @@
+export const API_KEY = "Vj9FzgHCzRiMV4_1Tc1SSsKYTfLepP30";
+export const API_TEST_KEY = "hQLsA78Yyjih5_SUWyRtOB8bmGZq_Z1e";
--- hype-dapp/src/pages/MyPage/index.js
+++ hype-dapp/src/pages/MyPage/index.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Container, Card, Avatar, Button } from "@nextui-org/react";
+import { useAccount } from "wagmi";
+import { API_KEY, API_TEST_KEY } from "../../constants/const";
+import { Network, Alchemy } from "alchemy-sdk";
+
+const settings = {
+  apiKey: API_TEST_KEY,
+  network: Network.ETH_GOERLI,
+};
 
 const MyPage = () => {
-  return <div></div>;
+  const { address, isConnected } = useAccount();
+  const [nfts, setNfts] = useState(null);
+
+  useEffect(() => {
+    const alchemy = new Alchemy(settings);
+    alchemy.nft.getNftsForOwner(address).then((res) => setNfts(res));
+  }, [address]);
+
+  return (
+    <Container>
+      {isConnected ? (
+        <>
+          <Avatar size="xl"></Avatar>
+          {address}
+          <Card>
+            <Card.Header>
+              <Button.Group color="gradient">
+                <Button>NFT</Button>
+                <Button>History</Button>
+              </Button.Group>
+            </Card.Header>
+            <Card.Body>
+              {nfts && nfts.totalCount === 0 ? "No items to display" : ""}
+            </Card.Body>
+          </Card>{" "}
+        </>
+      ) : (
+        <Card>
+          <Card.Body>지갑을 연결해주세요.</Card.Body>
+        </Card>
+      )}
+    </Container>
+  );
 };
 
 export default MyPage;
